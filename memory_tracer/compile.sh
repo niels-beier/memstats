7395dd33523e290b8bfe893cf958554372cea87d
niels-beier <shuzoyt@arcor.de>
2025-02-11 11:00:29 +0100

    Execute memory_tracer/compile.sh whenever CMake variable USE_MEMORY_TRACER is turned on to compile memory tracer

--- memory_tracer/compile.sh
+++ memory_tracer/compile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Current directory
-DIR=$PWD
+DIR=$(dirname "$(realpath $0)")
 PINTOOL_PATH=$1
 
 # Remove memorytracer.so if it exists
 rm -f $DIR/memorytracer.so
 
 # Copy files to the pintool directory
-cp -f $PWD/memorytracer.cc $PINTOOL_PATH/source/tools/ManualExamples/memorytracer.cpp
-cp -f $PWD/memorytracer.hh $PINTOOL_PATH/source/tools/ManualExamples/memorytracer.hh
+cp -f $DIR/memorytracer.cc $PINTOOL_PATH/source/tools/ManualExamples/memorytracer.cpp
+cp -f $DIR/memorytracer.hh $PINTOOL_PATH/source/tools/ManualExamples/memorytracer.hh
 
 # Compile the memory tracer
 cd $PINTOOL_PATH/source/tools/ManualExamples || exit
